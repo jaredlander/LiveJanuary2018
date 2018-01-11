c7aaba6d97fa470e731462f139bee5edb769e82f
Jared Lander <info@jaredlander.com>
2018-01-11 12:57:49 -0500

    first of dplyr

---
+++ code/DataManipulation.r
@@ -0,0 +1,37 @@
+library(dplyr)
+data(diamonds, package='ggplot2')
+diamonds
+
+head(diamonds)
+head(diamonds, n=3)
+
+tail(diamonds)
+tail(diamonds, n=2)
+
+tail(head(diamonds, n=4), n=1)
+
+head(diamonds)
+diamonds %>% head()
+diamonds %>% head(n=4)
+head(diamonds, n=4)
+
+tail(head(diamonds, n=4), n=1)
+diamonds %>% head(n=4) %>% tail(n=1)
+
+select(diamonds, carat, price)
+diamonds
+diamonds %>% select(carat, price)
+
+diamonds %>% filter(cut == 'Ideal')
+diamonds %>% filter(cut == 'Ideal' & price > 500)
+
+diamonds %>% slice(7)
+diamonds %>% slice(c(7, 13))
+diamonds %>% slice(7:13)
+
+diamonds %>% mutate(price / carat)
+diamonds
+diamonds %>% mutate(Ratio=price / carat)
+
+diamonds2 <- diamonds %>% mutate(Ratio=price/carat)
+diamonds2
