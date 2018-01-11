415d08cd91dac5b4582945c0432dffdb674367eb
Jared Lander <info@jaredlander.com>
2018-01-11 14:19:54 -0500

    added themes

--- code/Plotting.r
+++ code/Plotting.r
@@ -33,3 +33,36 @@ ggplot(diamonds, aes(x=carat, y=price)) +
 
 ggplot(diamonds, aes(x=carat, y=price)) + geom_point() + scale_y_log10()
 
+ggplot(diamonds, aes(x=price)) + geom_histogram()
+ggplot(diamonds, aes(x=price)) + geom_histogram(bins=50)
+ggplot(diamonds, aes(x=price)) + geom_histogram(bins=50, aes(fill=cut))
+
+ggplot(diamonds, aes(x=price)) + geom_density()
+ggplot(diamonds, aes(x=price)) + geom_density(color='red', fill='blue')
+
+ggplot(diamonds, aes(x=cut, y=price)) + geom_boxplot()
+ggplot(diamonds, aes(x=cut, y=price)) + geom_violin()
+?geom_violin
+
+# Fred Wilkerson: The Grammar of Graphics
+# Ed Tufte
+# Stephen Few
+# Andrew Gelman
+
+# junkcharts
+
+p <- ggplot(diamonds, aes(x=carat, y=price)) + geom_point(aes(color=cut))
+p
+
+p + geom_smooth()
+
+library(ggthemes)
+
+p + theme_economist()
+p + theme_economist() + scale_color_economist()
+p + scale_color_brewer()
+
+p + theme_wsj()
+p + theme_tufte()
+
+p + theme_excel() + scale_color_excel()
