5b6e4afc876421f9fe79782c8a73cffe1cdef40e
Jared Lander <info@jaredlander.com>
2018-01-11 11:51:16 -0500

    basic code

---
+++ code/Basics.r
@@ -0,0 +1,31 @@
+1 + 1
+
+4*3
+4/2
+4/3
+
+4 * 6 + 5
+4 * (6 + 5)
+
+x <- 2
+x
+
+y = 5
+y
+
+x <- 3
+x <- 3
+
+rm(y)
+
+theVar <- 17
+THEVAR
+
+x <- c(1, 2, 3)
+x
+
+x * 2
+sqrt(x)
+
+# this is a comment
+# multiplying x by 2 just printed the result, it did not change x
